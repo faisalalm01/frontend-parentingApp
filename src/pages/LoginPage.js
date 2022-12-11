5d2368d7bb63b64e13dab416c21be92153c4b58c
faisalalm01 <faisalali2858@gmail.com>
2022-12-11 18:13:23 +0700

    add to development

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styled from "styled-components";
 import { useNavigate, Link } from "react-router-dom";
-import Logo from "../assets/logo.svg";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { loginRoute } from "../utils/APIRoutes";
