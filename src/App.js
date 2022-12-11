fc21cb1a7d9f6e48731789b298c96bf584a7008f
faisalalm01 <faisalali2858@gmail.com>
2022-12-11 23:52:34 +0700

    add new dev

--- src/App.js
+++ src/App.js
@@ -1,41 +1,48 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from "react";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 
 import LandingPage from "./pages/LandingPage";
-import ArticlePage from './pages/ArticlePage';
-import ForumPage from './pages/ForumPage';
+import ArticlePage from "./pages/ArticlePage";
+import ForumPage from "./pages/ForumPage";
 import AboutPage from "./pages/AboutPage";
-import DetailPage from './pages/DetailPage';
+import DetailPage from "./pages/DetailPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-    return (
-        <section>
-            <header>
-                <Navigation />
-            </header>
+    // const navigate = useNavigate();
 
-            <main>
-                <Routes>
-                    <Route path="/login" element={<LoginPage/>}/>
-                    <Route path="/register" element={<RegisterPage/>}/>
-                    <Route path='/' element={<LandingPage />} />
-                    <Route path='/article' element={<ArticlePage />} />
-                    <Route path='/forum' element={<ForumPage />} />
-                    <Route path='/about' element={<AboutPage />} />
-                    <Route path='/articles/:id' element={<DetailPage />} />
-                </Routes>
-            </main>
+    // useEffect(async() =>{
+    //     if(!localStorage.getItem(pot))
+    // })
 
-            <div>
-                <Footer />
-            </div>
-        </section>
-    )
+      return (
+          <section>
+      <header>
+        <Navigation />
+      </header>
+
+      <main>
+        <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/article" element={<ArticlePage />} />
+          <Route path="/forum" element={<ForumPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/articles/:id" element={<DetailPage />} />
+        </Routes>
+      </main>
+
+      <div>
+        <Footer />
+      </div>
+    </section>
+  );
 }
 
 export default App;
