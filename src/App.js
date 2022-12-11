3b90cbdf8b22fbb0817acef8a07290083e040340
faisalalm01 <faisalali2858@gmail.com>
2022-12-12 00:28:57 +0700

    Revert "add chat"
    This reverts commit 7858bc1e896f180428a53e842e23e42219e24045.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
@@ -13,54 +12,27 @@ import Login from "./pages/LoginPage";
 import Register from "./pages/RegisterPage";
 
 function App() {
-  return (
-    <section>
-      <header>
-        <Navigation />
-      </header>
-      z
-      <main>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/article" element={<ArticlePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/articles/:id" element={<DetailPage />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </main>
-      <div>
-        <Footer />
-      </div>
-    </section>
-  );
-  }
+    return (
+        <section>
+            <header>
+                <Navigation />
+            </header>
+            <main>
+                <Routes>
+                    <Route path='/' element={<LandingPage />} />
+                    <Route path='/article' element={<ArticlePage />} />
+                    <Route path='/forum' element={<ForumPage />} />
+                    <Route path='/consultation' element={<ConsultationPage />} />
+                    <Route path='/about' element={<AboutPage />} />
+                    <Route path='/articles/:id' element={<DetailPage />} />
+                </Routes>
+            </main>
 
-  export default App;
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <div>
+                <Footer />
+            </div>
+        </section>
+    )
 }
 
 export default App;
->>>>>>> parent of 9240054 (refactory project)
